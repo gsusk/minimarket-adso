fd59196bb07c63778f9ecd44ba5c2e3836f5aa5a
gsus <satehortuamercado@gmail.com>
2026-01-14 20:03:46 -0500

    anadido orderdetails simple

--- src/main/java/org/adso/minimarket/dto/OrderDetails.java
+++ src/main/java/org/adso/minimarket/dto/OrderDetails.java
@@ -1,4 +1,19 @@
 package org.adso.minimarket.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class OrderDetails {
+    private UUID id;
+    private String email;
+    private Long userId;
+    private String status;
+    private LocalDateTime createdAt;
 }
