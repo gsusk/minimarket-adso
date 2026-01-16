de7db3931d7d8abf6319aca62a42394d8600e0e2
gsus <satehortuamercado@gmail.com>
2026-01-15 23:13:27 -0500

    anadido mapeo post al controller

--- src/main/java/org/adso/minimarket/controller/api/CheckoutControllerImpl.java
+++ src/main/java/org/adso/minimarket/controller/api/CheckoutControllerImpl.java
@@ -5,6 +5,7 @@ import org.adso.minimarket.dto.CreatePaymentRequest;
 import org.adso.minimarket.service.CheckoutService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,6 +18,7 @@ public class CheckoutControllerImpl implements CheckoutController {
     }
 
     @Override
+    @PostMapping("/checkout/initialize")
     public ResponseEntity<?> initializeCheckout(
             @AuthenticationPrincipal UserPrincipal userPrincipal,
             @RequestBody(required = false) CreatePaymentRequest body) {
