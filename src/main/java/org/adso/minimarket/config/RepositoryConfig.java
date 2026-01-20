33a178745cb106394beec9cdbdd465b66c3dadec
gsus <satehortuamercado@gmail.com>
2026-01-20 16:25:49 -0500

    anadido configuracion para compatibilidad de repositorio de busqueda

--- src/main/java/org/adso/minimarket/config/RepositoryConfig.java
+++ src/main/java/org/adso/minimarket/config/RepositoryConfig.java
@@ -1,10 +1,15 @@
 package org.adso.minimarket.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
 @Configuration
 @EnableJpaRepositories(
-        basePackages = "org.adso.minimarket.repository.jpa" // Put SQL repos here
+        basePackages = "org.adso.minimarket.repository.jpa"
 )
 @EnableElasticsearchRepositories(
-        basePackages = "org.adso.minimarket.repository.elastic" // Put ES repos here
+        basePackages = "org.adso.minimarket.repository.elastic"
 )
 public class RepositoryConfig {
 }
