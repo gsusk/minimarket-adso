05f1b62bdf4782378b0ac5126e34b2650eb443d3
gsus <satehortuamercado@gmail.com>
2026-01-21 20:26:04 -0500

    formateado

--- src/main/java/org/adso/minimarket/event/ProductCreatedEventListener.java
+++ src/main/java/org/adso/minimarket/event/ProductCreatedEventListener.java
@@ -29,7 +29,7 @@ public class ProductCreatedEventListener {
                 p.getStock(),
                 p.getAttributes(),
                 p.getCreatedAt()
-                );
+        );
         searchService.saveIndex(productDocument);
     }
 }
