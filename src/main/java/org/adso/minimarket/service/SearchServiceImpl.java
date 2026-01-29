94fd9a862bb092443df41cfd778c1d29f4c9322f
gsus <satehortuamercado@gmail.com>
2026-01-29 18:58:22 -0500

    anadido elasticoperations para queries dinamicas

--- src/main/java/org/adso/minimarket/service/SearchServiceImpl.java
+++ src/main/java/org/adso/minimarket/service/SearchServiceImpl.java
@@ -3,14 +3,17 @@ package org.adso.minimarket.service;
 import org.adso.minimarket.dto.SearchFilters;
 import org.adso.minimarket.models.document.ProductDocument;
 import org.adso.minimarket.repository.elastic.ProductSearchRepository;
+import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
 import org.springframework.stereotype.Service;
 
 @Service
 public class SearchServiceImpl implements SearchService {
     private final ProductSearchRepository searchRepository;
+    private final ElasticsearchOperations operations;
 
-    public SearchServiceImpl(ProductSearchRepository searchRepository) {
+    public SearchServiceImpl(ProductSearchRepository searchRepository, ElasticsearchOperations operations) {
         this.searchRepository = searchRepository;
+        this.operations = operations;
     }
 
     @Override
