2324dee170f2600233b7e8fa59bcbd5f2a3a38d8
gsus <satehortuamercado@gmail.com>
2026-01-29 14:29:59 -0500

    arreglo de mapper para manejar el nuevo nombre 'firstName'

--- src/main/java/org/adso/minimarket/mappers/UserMapper.java
+++ src/main/java/org/adso/minimarket/mappers/UserMapper.java
@@ -3,8 +3,10 @@ package org.adso.minimarket.mappers;
 import org.adso.minimarket.dto.BasicUser;
 import org.adso.minimarket.models.user.User;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
+    @Mapping(target = "firstName", source = "name")
     BasicUser toResponseDto(User user);
 }
