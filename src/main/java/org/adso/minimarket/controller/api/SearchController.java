813b1c2f5ff90dbcf0b21f3052f51607a975b613
gsus <satehortuamercado@gmail.com>
2026-01-30 23:09:20 -0500

    anadido search products inicial para probar la busqueda

--- src/main/java/org/adso/minimarket/controller/api/SearchController.java
+++ src/main/java/org/adso/minimarket/controller/api/SearchController.java
@@ -1,4 +1,14 @@
 package org.adso.minimarket.controller.api;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.math.BigDecimal;
+import java.util.Optional;
+
 public interface SearchController {
+     ResponseEntity<?> searchProducts(String query,
+                                      Optional<String> brand,
+                                      Optional<BigDecimal> minValue,
+                                      Optional<BigDecimal> maxValue);
 }
--- src/main/java/org/adso/minimarket/controller/api/SearchControllerImpl.java
+++ src/main/java/org/adso/minimarket/controller/api/SearchControllerImpl.java
@@ -1,4 +1,32 @@
 package org.adso.minimarket.controller.api;
 
-public class SearchControllerImpl {
+import org.adso.minimarket.service.SearchService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.math.BigDecimal;
+import java.util.Optional;
+
+@Controller
+public class SearchControllerImpl implements SearchController {
+    private static final Logger log = LoggerFactory.getLogger(SearchControllerImpl.class);
+    private final SearchService searchService;
+
+    public SearchControllerImpl(SearchService searchService) {
+        this.searchService = searchService;
+    }
+
+    @Override
+    @GetMapping("/search")
+    public ResponseEntity<?> searchProducts(@RequestParam("q") String query,
+                               @RequestParam("brand") Optional<String> brand,
+                               @RequestParam("min") Optional<BigDecimal> minValue,
+                               @RequestParam("max") Optional<BigDecimal> maxValue) {
+        log.info("query: {}\nbrand: {}\nmin: {}\nmax: {}",query, brand, minValue, maxValue);
+        return ResponseEntity.ok("");
+    }
 }
