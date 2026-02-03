7efed6ebaaea15dbde0f6e62c0601a168742bd10
gsus <satehortuamercado@gmail.com>
2026-02-03 11:36:43 -0500

    arreglados los filtros para que no se sobreescriban

--- src/main/java/org/adso/minimarket/service/SearchServiceImpl.java
+++ src/main/java/org/adso/minimarket/service/SearchServiceImpl.java
@@ -1,17 +1,17 @@
 package org.adso.minimarket.service;
 
+import co.elastic.clients.elasticsearch._types.aggregations.Aggregation;
 import co.elastic.clients.elasticsearch._types.aggregations.AggregationBuilders;
+import co.elastic.clients.elasticsearch._types.query_dsl.BoolQuery;
 import lombok.extern.slf4j.Slf4j;
 import org.adso.minimarket.dto.SearchFilters;
 import org.adso.minimarket.models.document.ProductDocument;
 import org.adso.minimarket.repository.elastic.ProductSearchRepository;
 import org.springframework.data.elasticsearch.client.elc.ElasticsearchAggregations;
 import org.springframework.data.elasticsearch.client.elc.NativeQuery;
-import org.springframework.data.elasticsearch.client.elc.NativeQueryBuilder;
 import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
 import org.springframework.data.elasticsearch.core.SearchHit;
 import org.springframework.data.elasticsearch.core.SearchHits;
-import org.springframework.data.elasticsearch.core.query.Query;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
@@ -35,33 +35,27 @@ public class SearchServiceImpl implements SearchService {
 
     @Override
     public List<ProductDocument> searchWithFilters(SearchFilters filters, String query) {
-        NativeQueryBuilder natQuery = NativeQuery.builder()
-                .withQuery(q -> q
-                        .match(m -> m
-                                .field("name")
-                                .fuzziness("1")
-                                .query(query)
-                        )
-                );
+        BoolQuery.Builder bool = new BoolQuery.Builder();
+        bool.must(mu -> mu
+                .match(m -> m
+                        .field("name")
+                        .query(query)
+                        .fuzziness("1")
+                )
+        );
+
 
         if (filters.getBrand() != null) {
-            natQuery.withQuery(q -> q
-                    .bool(b -> b
-                            .filter(f -> f
-                                    .term(t -> t
-                                            .field("specifications.brand")
-                                            .value(filters.getBrand())
-                                    )
-                            )
+            bool.filter(f -> f
+                    .term(t -> t
+                            .field("specifications.brand")
+                            .value(filters.getBrand())
                     )
             );
         }
 
-        natQuery.withAggregation("min_price", AggregationBuilders.min(m -> m.field("price")))
-                .withAggregation("max_price", AggregationBuilders.max(m -> m.field("price")));
-
         if (filters.getMinPrice() != null && filters.getMinPrice().compareTo(BigDecimal.ZERO) > 0) {
-            natQuery.withFilter(f -> f
+            bool.filter(f -> f
                     .range(r -> r
                             .number(n -> n
                                     .field("price")
@@ -71,8 +65,10 @@ public class SearchServiceImpl implements SearchService {
             );
         }
 
-        if (filters.getMaxPrice() != null && filters.getMaxPrice().compareTo(BigDecimal.ZERO) > 0) {
-            natQuery.withFilter(f -> f
+        if (filters.getMaxPrice() != null
+                && filters.getMaxPrice().compareTo(BigDecimal.ZERO) > 0
+                && filters.getMaxPrice().compareTo(filters.getMinPrice()) > -1) {
+            bool.filter(f -> f
                     .range(r -> r
                             .number(n -> n
                                     .field("price")
@@ -82,8 +78,15 @@ public class SearchServiceImpl implements SearchService {
             );
         }
 
+        Aggregation min = AggregationBuilders.min(m -> m.field("price"));
+        Aggregation max = AggregationBuilders.max(m -> m.field("price"));
 
-        Query searchQuery = natQuery.withMaxResults(20).build();
+        NativeQuery searchQuery = NativeQuery.builder()
+                .withQuery(q -> q.bool(bool.build()))
+                .withAggregation("min_price", min)
+                .withAggregation("max_price", max)
+                .withMaxResults(20)
+                .build();
 
         SearchHits<ProductDocument> searchHits = operations.search(searchQuery, ProductDocument.class);
 
@@ -100,7 +103,6 @@ public class SearchServiceImpl implements SearchService {
         log.info("search result: {}", searchHits);
         for (SearchHit<ProductDocument> searchHit : searchHits) {
             ProductDocument pd = searchHit.getContent();
-            log.info("name: {}", pd.getName());
             log.info("brand: {}", pd.getBrand());
             log.info("price: {}", pd.getPrice());
             log.info("=================================");
